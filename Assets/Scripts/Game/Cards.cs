f51c907a9c60accba348b7000fa6645d6bd0e086
Lavion <lavion@invalsia.com>
2025-12-03 05:09:39 +0100

    Implement the card stack (#1)
    * Add .gitignore file
    * Initialize project files & update dependencies
    * G: The Card Stack

--- Assets/Scripts/Game/Cards.cs
+++ Assets/Scripts/Game/Cards.cs
@@ -9,32 +9,23 @@ namespace Game
         public abstract class Card
         {
             #region Properties
-
             public abstract string Name { get; }
-
             public abstract int ManaCost { get; }
-
             public abstract string IconName { get; }
-
             #endregion
 
             public abstract List<(Vector2Int, Color32)> GetPreviewEffect(Vector2Int vMouseCoord);
-
             public abstract void Perform(Vector2Int vMouseCoord);
         }
 
         private static Cards    sm_instance;
-
+        private readonly Stack<Card>    m_drawCards     = new Stack<Card>();
+        private readonly Stack<Card>    m_discardCards  = new Stack<Card>();
+        
         #region Properties
-
-        // TODO: Create and store a Stack and return here
-        public Stack<Card> DrawCards => throw new System.NotImplementedException();
-
-        // TODO: Create and store a Stack and return here
-        public Stack<Card> DiscardCards => throw new System.NotImplementedException();
-
+        public Stack<Card> DrawCards => m_drawCards;
+        public Stack<Card> DiscardCards => m_discardCards;
         public static Cards Instance => sm_instance;
-
         #endregion
 
         private void Awake()
@@ -44,8 +35,8 @@ namespace Game
 
         public void InitializeCards()
         {
-            // NOTHING TO DO IN THIS FUNCTION... Leave as is :)
-
+            //// NOTHING TO DO IN THIS FUNCTION... Leave as is :)
+            // ! nothing to do my ASS     (╯°□°)╯︵ ┻━┻
             try
             {
                 // find all card types
@@ -57,17 +48,14 @@ namespace Game
                 {
                     System.Type t = cardTypes[i % cardTypes.Count];
                     Card card = System.Activator.CreateInstance(t) as Card;
-                    if (card != null)
-                    {
-                        cards.Add(card);
-                    }
+                    if (card != null) cards.Add(card);
                 }
 
                 // shuffle cards
                 for (int i = 0; i < cards.Count; ++i)
                 {
-                    int i1 = Random.Range(0, DrawCards.Count);
-                    int i2 = Random.Range(0, DrawCards.Count);
+                    int i1 = Random.Range(0, cards.Count);
+                    int i2 = Random.Range(0, cards.Count);
                     if (i1 != i2)
                     {
                         Card tmp = cards[i1];
@@ -77,39 +65,53 @@ namespace Game
                 }
 
                 // push onto draw stack
-                foreach (Card card in cards)
-                {
-                    DrawCards.Push(card);
-                }
+                foreach (Card card in cards) DrawCards.Push(card);
             }
             catch (System.NotImplementedException)
             {
                 Debug.Log("Looks like you have some work to do?");
+                // Nuh-uh
             }
         }
 
         public Card DrawCard()
         {
-            // TODO: get the next card on the DrawCards stack and return it
-            // (removing it from the stack in the process)
-            throw new System.NotImplementedException();
+            // TODO: 
+            //// get the next card on the DrawCards stack and return it
+            //// (removing it from the stack in the process)
+            if (DrawCards.Count == 0)return null;
+            return DrawCards.Pop();
         }
 
         public void DiscardCard(Card card)
         {
-            // TODO: push the card being passed in to this function
-            // on the DiscardStack
-            throw new System.NotImplementedException();
+            // TODO: 
+            //// push the card being passed in to this function on the DiscardStack
+            if (card == null) return;
+            DiscardCards.Push(card);
         }
 
         public void ShuffleDeck()
         {
             // TODO: 
-            // 1. Add ALL the cards from the DiscardStack into a new temporary List
-            // 2. Shuffle the new list by randomly swapping elements in the list 100 times
-            // 3. Clear the DiscardCards stack
-            // 4. Add all the Cards from your temporary (now shuffled) list into the DrawStack
-            throw new System.NotImplementedException();
+            //// 1. Add ALL the cards from the DiscardStack into a new temporary List
+            //// 2. Shuffle the new list by randomly swapping elements in the list 100 times
+            //// 3. Clear the DiscardCards stack
+            //// 4. Add all the Cards from your temporary (now shuffled) list into the DrawStack
+            if (DiscardCards.Count == 0) return;
+            List<Card> shuffled = new List<Card>(DiscardCards);
+            for (int i = 0; i < 100 && shuffled.Count > 1; ++i)
+            {
+                int i1 = Random.Range(0, shuffled.Count);
+                int i2 = Random.Range(0, shuffled.Count);
+                if (i1 == i2) continue;
+                Card temp = shuffled[i1];
+                shuffled[i1] = shuffled[i2];
+                shuffled[i2] = temp;
+            }
+
+            DiscardCards.Clear();
+            foreach (Card card in shuffled) DrawCards.Push(card);
         }
     }
 }
